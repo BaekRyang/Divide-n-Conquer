fb7e546e7e702ecae94341d6dbe415b557f6f1ae
BaekRyang <graywolf147@gmail.com>
2024-03-06 15:32:53 +0900

    RandomDataMaker

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Divide_n_Conquer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (int _i in RandomDataMaker.MakeData<int>(10, true))
+            {
+                Console.WriteLine(_i);
+            }
+        }
+    }
+}
---
+++ RandomDataMaker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Divide_n_Conquer
+{
+    public class RandomDataMaker
+    {
+        private const  int    MAX_NUM_SIZE = 100;
+        private static Random _random;
+
+        public static IEnumerable<T> MakeData<T>(int _size, bool _allowDuplicates = false)
+        {
+            Type _type = typeof(T);
+
+            switch (_type)
+            {
+                case Type _t when _t == typeof(int):
+                {
+                    return MakeIntData<T>(_size, _allowDuplicates);
+                }
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static IEnumerable<T> MakeIntData<T>(int _size, bool _allowDuplicates)
+        {
+            if (_size <= 0 || _size > MAX_NUM_SIZE) throw new ArgumentOutOfRangeException(nameof(_size));
+            _random = new Random();
+
+            //임시 데이터 생성
+            ICollection<int> _data = Enumerable.Range(1, MAX_NUM_SIZE).ToList();
+            
+            if (_allowDuplicates)
+            {
+                Console.WriteLine("중복 허용");
+                for (int i = 0; i < _size; i++) 
+                    yield return (T) Convert.ChangeType(_random.Next(1, MAX_NUM_SIZE), typeof(T));
+            }
+            else
+            {
+                Console.WriteLine("중복 불가");
+                for (int i = 0; i < _size; i++)
+                {
+                    int _randomData = _data.ElementAt(_random.Next(0, _data.Count));
+                    _data.Remove(_randomData);
+                    yield return (T) Convert.ChangeType(_randomData, typeof(T));
+                }
+            }
+        }
+    }
+}
